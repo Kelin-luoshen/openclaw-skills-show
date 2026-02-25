9cbe8d846211fa31a7753b3b348a5434512161f2
Kelin-luoshen <canin.xiong@gmail.com>
2026-02-25 23:14:59 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+// ES 模块下的路径修复方案
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 export default defineConfig({
+  plugins: [react()],
   root: 'client',
+  resolve: {
+    alias: {
+      // 告诉 Vite，看到 @ 就去 client/src 里面找！
+      '@': path.resolve(__dirname, './client/src')
+    }
+  },
   build: {
     outDir: '../dist',
     emptyOutDir: true
