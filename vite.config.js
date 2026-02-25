af60c424f7d0f37c6d59e7040985b5ebe660bf5b
Kelin-luoshen <canin.xiong@gmail.com>
2026-02-25 23:35:47 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,17 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 import { fileURLToPath } from 'url'
 
-// ES 模块下的路径修复方案
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 export default defineConfig({
   plugins: [react()],
-  root: 'client',
+  // 核心1：告诉 Vite 网页入口在 client 文件夹
+  root: 'client', 
   resolve: {
     alias: {
-      // 告诉 Vite，看到 @ 就去 client/src 里面找！
-      '@': path.resolve(__dirname, './client/src')
+      // 核心2：告诉 Vite，代码里的 @ 代表主根目录下的 client/src
+      '@': path.resolve(__dirname, './client/src') 
     }
   },
   build: {
